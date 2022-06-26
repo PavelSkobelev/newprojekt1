2b550b56609db445a1db87759ba7f8bf151899d7
PavelSkobelev <psckobelev@yandex.by>
2022-06-26 15:10:10 +0300

    new test commid

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,87 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class User {
     private int id;
     private String name;
     private int age;
     private Sex sex;
+    private static Map<Integer, User> allUsers;
+    private static int countId = 0;
+
+    public User(String name, int age, Sex sex) {
+        if (allUsers == null) {
+            allUsers = new HashMap<>();
+        }
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        if (!hasUser()) {
+            countId++;
+            this.id = countId;
+            allUsers.put(id, this);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    private boolean hasUser() {
+        for (User user : allUsers.values()) {
+            if (user.equals(this) && user.hashCode() == this.hashCode()) {
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                age == user.age &&
+                Objects.equals(name, user.name) &&
+                sex == user.sex;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, sex);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
 }
